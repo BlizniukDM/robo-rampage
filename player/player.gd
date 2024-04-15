c4fb0d10515b1d0dbec5aacaa2fcec65b6a89db7
Danila <danila.blizniuk@gmail.com>
2024-04-15 17:15:34 +0300

    Added the smooth_camera

--- player/player.gd
+++ player/player.gd
@@ -4,11 +4,20 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+
+@onready var camera_pivot: Node3D = $CameraPivot
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_motion = Vector2.ZERO
 
+
+func _ready() -> void:
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+    handle_camera_rotation()
     # Add the gravity.
     if not is_on_floor():
         velocity.y -= gravity * delta
@@ -29,3 +38,20 @@ func _physics_process(delta: float) -> void:
         velocity.z = move_toward(velocity.z, 0, SPEED)
 
     move_and_slide()
+    
+func _input(event: InputEvent) -> void:
+    if event is InputEventMouseMotion:
+        if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+            mouse_motion = -event.relative * 0.001
+    if event.is_action_pressed("ui_cancel"):
+        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+        
+func handle_camera_rotation() -> void:
+    rotate_y(mouse_motion.x)
+    camera_pivot.rotate_x(mouse_motion.y)
+    camera_pivot.rotation_degrees.x = clampf(
+        camera_pivot.rotation_degrees.x,
+        -90.0,
+         90.0
+    )
+    mouse_motion = Vector2.ZERO 
