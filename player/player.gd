fb82dadfcae6118ca2c2a1c624d403d7ee8cc6e3
Danila <danila.blizniuk@gmail.com>
2024-04-16 17:10:09 +0300

    Jump_height

--- player/player.gd
+++ player/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
+@export var jump_height : float
 
 @onready var camera_pivot: Node3D = $CameraPivot
 
@@ -24,7 +24,7 @@ func _physics_process(delta: float) -> void:
 
     # Handle jump.
     if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+        velocity.y = sqrt(jump_height * 2 * gravity)
 
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
