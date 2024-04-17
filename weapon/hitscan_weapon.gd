00c88ca6c4b7ffdfa4fbd0e82b6f2cb872f266ee
Danila <danila.blizniuk@gmail.com>
2024-04-17 16:13:11 +0300

    Firing function

--- weapon/hitscan_weapon.gd
+++ weapon/hitscan_weapon.gd
@@ -2,14 +2,24 @@ extends Node3D
 
 
 @export var fire_rate : float
+@export var recoil : float = 0.05
+@export var weapon_mesh : Node3D
 
 
 @onready var cooldown: Timer = $Cooldown
-
+@onready var weapon_position : Vector3 = weapon_mesh.position
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 
 func _process(delta: float) -> void:
     if Input.is_action_pressed("fire"):
         if cooldown.is_stopped():
-            cooldown.start(1.0 / fire_rate)
-            print ("Weapon fired!")
+            shoot()
+        
+    weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
+
+
+func shoot() -> void:
+    cooldown.start(1.0 / fire_rate)
+    printt("Weapon fired!", ray_cast_3d.get_collider())
+    weapon_mesh.position.z += recoil
